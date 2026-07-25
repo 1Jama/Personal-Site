a75dc6da289ae2a05db986f6263bac6159c34e0d
1Jama <Jamaf@my.yorku.ca>
2026-07-25 02:10:16 -0400

    Fix dependency resolution

--- src/Components/ProjectData.js
+++ src/Components/ProjectData.js
@@ -5,7 +5,7 @@ export const ProjectData = [
     imgSrc: spanThumbnail,
     title: 'Spanish Guesser',
     description:
-      'Dynamic React app to practise basic levels of the Spanish language. Contains two levels of difficulty tallying score on right answers. Robost backend made using MongoDB and Node.Js, as well as utilizing multiple Restful APIs for things like determining corectness in hard mode.',
+      'Dynamic React app to practise basic levels of the Spanish language. Contains two levels of difficulty tallying score on right answers. Robust backend made using MongoDB and Node.Js, as well as utilizing multiple Restful APIs for things like determining correctness in hard mode.',
     technology: 'React React-Hooks Node.JS MongoDB Restful-APIs HTML CSS',
     url: 'https://spanishguesser.com/Guess',
     gitUrl: 'https://github.com/1Jama/guess-the-word',
@@ -14,7 +14,7 @@ export const ProjectData = [
     imgSrc: portThumbnail,
     title: 'Personal Portfolio Website',
     description:
-      'Website where I host my projects as well as have information listed about myself. Built using React, Bootstrap, Tailwind',
+      'Website where I host my projects as well as have information listed about myself. Built using React, Bootstrap, Tailwind.',
     technology: 'React React-Hooks Bootstrap Tailwind HTML CSS',
     url: 'https://jamafarah.com/',
     gitUrl: 'https://github.com/1Jama/Personal-Site',
